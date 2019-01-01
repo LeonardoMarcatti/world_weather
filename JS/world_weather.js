b4448e593b18160d2bbee7b64165015177f7ef1c
Leonardo Marcatti da Silva <leonardomarcatti@hotmail.com>
2019-01-01 21:33:40 -0200

    Add files via upload

--- JS/world_weather.js
+++ JS/world_weather.js
@@ -1,42 +1,41 @@
 $('#check').on('click',
-    function GetWeather(e) {
+    async function GetWeather(e) {
         e.preventDefault();
-        $('#results').html("");
         let city = $('#city').val();
+        var imagem;
+        $('#data').html(' ');
         $.ajax({
+            method: "get",
+            url: `https://api.pexels.com/v1/search?query=${city}`,
+            headers: {"Authorization": "563492ad6f91700001000001543dd7e9b70d4889849f4177cdab5355"},
+            success: function(data) {
+                console.log(data);                
+            imagem = data.photos[0].src.large;
+            return imagem;
+           }
+        })
+        setTimeout(() => { $.ajax({
             method: "GET",
             url: `http://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&APPID=9988132bfa41c2e199b9f2eedeb37574&`,
             success: function (response) {
-                console.log(response);
-                $('#results').append(
-                        `<h5> Weather forecast for: ${response.list[0].name} </h5><br>
-                        <i class="fas fa-temperature-low"></i>: ${response.list[0].main.temp}C° <br>
-                        <i class="fas fa-tint"></i> ${response.list[0].main.humidity}% <br>
-                        <i class="fas fa-city"></i> ${response.list[0].weather[0].main}/${response.list[0].weather[0].description} <br>
-                        <i class="fas fa-wind"></i> ${response.list[0].wind.speed}km/h <br>
-                        <i class="far fa-compass"></i> ${response.list[0].wind.deg}°`
-                    )
+                $('#data').append(
+                    `<h3 style="text-align: center"> Weather forecast for: ${response.list[0].name}</h3><br>
+                    <i class="fas fa-temperature-low fa-2x"> ${response.list[0].main.temp}C°</i>
+                    <i class="fas fa-tint fa-2x"> ${response.list[0].main.humidity}%</i><br>
+                    <i class="fas fa-city fa-2x"> ${response.list[0].weather[0].main}/${response.list[0].weather[0].description}</i><br>
+                    <i class="fas fa-wind fa-2x"> ${response.list[0].wind.speed}km/h</i> 
+                    <i class="far fa-compass fa-2x"> ${response.list[0].wind.deg}</i><br>`
+                ).css('background-image', `url(${imagem})`);
             }
         })
-    }
-);
-
-
-function image(place) {
-    $.ajax({
-        method: "get",
-        url: `https://api.pexels.com/v1/search?query=${place}`,
-        headers: {"Authorization": "563492ad6f91700001000001543dd7e9b70d4889849f4177cdab5355"},
-        success: function (response) {
-        console.log(response);        
-        }
-    });
+    }, 500);
 }
-image("berlin");
+)
 
-
-async function getPhoto() { 
-    await fetch("https://api.pexels.com/v1/search?query=berlin", {
+/*
+<img src="${imagem}" />
+async function getPhoto(place) { 
+    await fetch(`https://api.pexels.com/v1/search?query=${place}`, {
     method:'get',
     headers: {"Authorization": "563492ad6f91700001000001543dd7e9b70d4889849f4177cdab5355",}
     })
@@ -44,10 +43,12 @@ async function getPhoto() {
     .then(result => console.log(result))
     .catch(error => console.log(error))
 }
-getPhoto();
+getPhoto('Toronto');
 
+$('#check').on('click', function() { 
 let MyPhoto = new Promise((resolve, reject) =>{
-    fetch("https://api.pexels.com/v1/search?query=berlin", {
+    let place = $(city).val();
+    fetch(`https://api.pexels.com/v1/search?query=${place}`, {
     method:'get',
     headers: {"Authorization": "563492ad6f91700001000001543dd7e9b70d4889849f4177cdab5355",}
     })
@@ -55,3 +56,5 @@ let MyPhoto = new Promise((resolve, reject) =>{
     .then(result => console.log(result))
     .catch(error => console.log(error))
 });
+})
+*/
